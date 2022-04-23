afa5da58d27176835fbc0a48f80a43c40ab1a02e
Mr Whipp <dwhipp88@gmail.com>
2022-04-23 00:19:20 -0600

    The event loop in practice

--- event-loop.js
+++ event-loop.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const crypto = require("crypto");
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 2;
 
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
@@ -9,11 +12,24 @@ fs.readFile("test-file.txt", () => {
     "----------------------------------------------------------------"
   );
 
-  setImmediate(() => console.log("Timer 2 finished"), 0);
-  setImmediate(() => console.log("Timer 3 finished"), 3000);
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
   setImmediate(() => console.log("Immediate 2 finished"));
 
   process.nextTick(() => console.log("Process.nextTick finished"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
 });
 
 console.log("Hello from top level code!");
